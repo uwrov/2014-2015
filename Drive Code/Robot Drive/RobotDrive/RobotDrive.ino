da3b1447bcecb82f1515173c0de0f1c8e97830ca
Max Shao <max.shao@hotmail.com>
2015-03-03 17:46:29 -0800

    Added special Ping packets (id 102) and LED packets(id 101) and changed constants to all caps

--- Drive Code/Robot Drive/RobotDrive/RobotDrive.ino
+++ Drive Code/Robot Drive/RobotDrive/RobotDrive.ino
@@ -1,14 +1,21 @@
-#define motors 6
-#define sensors 6
 #define delayCounter 5
+
+// Special LED packets
+const int keyLight = 101;
+const int ledPin = 13;
+boolean ledState = LOW;
+
+// Special Ping packets
+const int keyPing = 102;
 
 // Motor ports (D for direction and P for power)
 // Motor keys (first compare 1 then 2)
+#define motors 6
 int motorPortsD[motors] = {
-    0, 1, 2, 3, 4, 5
+    1, 2, 3, 4, 5, 6
 };
 int motorPortsP[motors] = {
-    6, 7, 8, 9, 10, 11
+    7, 8, 9, 10, 11, 12
 };
 int motorPower[motors] = {
     0, 0, 0, 0, 0
@@ -18,9 +25,11 @@ int changeRate = 8;
 int keyIn1 = 17; // 1st key for reading motor data
 int keyIn2 = 151; // 2nd key for reading motor data
 
+
 // Sensor ports and every sensor iteration to send
 // sensor value
 // Sensor key values to be sent
+#define sensors 6
 int sensorIteration = 1;
 int sensorPorts[sensors] = {
     0, 1, 2, 3, 4, 5
@@ -31,6 +40,8 @@ int keyOut2 = 225; // 2nd key for sending sensor data
 // Counter for sensor iteration
 int counter = 0;
 
+
+
 // Initialize serial and output ports
 void setup() {
     Serial.begin(9600);
@@ -38,26 +49,48 @@ void setup() {
         pinMode(motorPortsD[i], OUTPUT);
         pinMode(motorPortsP[i], OUTPUT);
     }
+    pinMode(ledPin, OUTPUT);
 }
+
+
 
 void loop() {
     // read and turn motors from Serial port
     while (Serial.available() >= 5) {
         if (Serial.read() == keyIn1 && Serial.read() == keyIn2) {
-            int motorNum = Serial.read();
-            int motorP = Serial.read();
-            int motorD = Serial.read();
-            if (motorD > 0 && motorD < 3) {
-                // stores the read motorP and motorD as the targetted
-                // motor power (btw -256 and 256)
-                if (motorD == 1) {
-                    motorPower[motorNum] = -1 * motorP;
-                } else if (motorD == 2) {
-                    motorPower[motorNum] = motorP;
+            int specialKey = Serial.read();
+            if (specialKey == keyPing) {
+               Serial.write(keyPing);
+               int pingPkt = Serial.read();
+               Serial.write(pingPkt);
+               Serial.read();
+            } else if (specialKey == keyLight) {
+                // if the LED is off turn it on and vice-versa:
+                if (ledState == LOW) {
+                    ledState = HIGH;
+                } else {
+                    ledState = LOW;
+                }
+                // set the LED with the ledState of the variable:
+                digitalWrite(ledPin, ledState);
+            } else { 
+                int motorNum = Serial.read();
+                int motorP = Serial.read();
+                int motorD = Serial.read();
+                if (motorD > 0 && motorD < 3) {
+                    // stores the read motorP and motorD as the targetted
+                    // motor power (btw -256 and 256)
+                    if (motorD == 1) {
+                        motorPower[motorNum] = -1 * motorP;
+                    } else if (motorD == 2) {
+                        motorPower[motorNum] = motorP;
+                    }
                 }
             }
         }
     }
+    
+    
     for (int i = 0; i < motors; i++) {
         // updates motor power to be midpoint btw current power and 
         // targeted power
@@ -72,6 +105,8 @@ void loop() {
         analogWrite(motorPortsP[i], motorP);
         digitalWrite(motorPortsD[i], motorD);
     } 
+    
+    
     counter++;
     // every sensorIteration iteration, reads and sends sensor
     // reading
