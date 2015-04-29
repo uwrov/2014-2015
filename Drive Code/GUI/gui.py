a155c9f06370593655088524431804e4af34fa3c
Iain Bromley <iain.b@sbcglobal.net>
2015-04-29 00:17:13 -0700

    Reformatted GUI, removes a descent number of mystery numbers, numerical displays on charts.

--- Drive Code/GUI/gui.py
+++ Drive Code/GUI/gui.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2, math
 import RobotSurfaceControl as rc
 import pygame as pg
 import numpy
@@ -9,17 +9,26 @@ pg.init()
 # colors
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+
+THRESHOLD = 0.15
 
 camera_index = 0
 camera=cv2.VideoCapture(camera_index)
+camera.set(3, 480)
+camera.set(4, 640)
 
-SCREEN_SIZE = (1080, 520)
+SCREEN_SIZE = (1180, 520)
 CAM_VIEW_SIZE = (320, 180)
 
 x = 0;
 y = 0;
+z = 0;
 speed = 0;
 rotation = 0;
+
+font = pg.font.SysFont(None, 20)
 
 
 class GuiPrint(object):
@@ -62,17 +71,22 @@ def joy_init():
 
 
 def update_values():
-    global x, y, speed, rotation, joystickEnabled
+    global x, y, z, speed, rotation, joystickEnabled
     if pg.joystick.get_count() == 0:
         joystickEnabled = False
-        x = (pg.mouse.get_pos()[0] - SCREEN_SIZE[0]/2.0) / (SCREEN_SIZE[0] / 2.0)
-        y = -((-(pg.mouse.get_pos()[1] - SCREEN_SIZE[1])/2.0) / (SCREEN_SIZE[1] / 2.0))
+        x = float(pg.mouse.get_pos()[0]) / (SCREEN_SIZE[0]/2.0) - 1
+        y = 1 - float(pg.mouse.get_pos()[1]) / (SCREEN_SIZE[1]/2.0)
     else:
         joystickEnabled = True
         x = joystick.get_axis(0)
-        y = joystick.get_axis(1)
-        speed = joystick.get_axis(2)
-        rotation = joystick.get_axis(3)
+        y = -joystick.get_axis(1)
+        z = joystick.get_axis(2)
+        rotation = joystick.get_axis(4)
+        if (pow((pow(x,2) + pow(y,2)),0.5) < 1):
+            speed = pow((pow(x,2) + pow(y,2)),0.5)
+        else:
+            speed = 1
+        keepValuesAtZero()
        
 
 def getCamFrame(camera):
@@ -85,9 +99,87 @@ def getCamFrame(camera):
 def blitCamFrame(frame,screen):
     screen.blit(frame,(10,10))
     return screen
+
+def getStringDir():
+    global x, y
+    if (x == 0):
+        if (y > 0): return "N"
+        else: return "S"
+    elif (y == 0):
+        if (x > 0): return "E"
+        else: return "W"
+    elif (x < 0):
+        if (y > 0): return "NW"
+        else: return "SW"
+    elif (x > 0):
+        if (y > 0): return "NE"
+        else: return "SE"
+
+
+def drawDirection(screen, origin_x, origin_y, radius):
+    global x, y
+    pg.draw.ellipse(screen, BLACK, [origin_x, origin_y, radius * 2, radius * 2], 5)
+    normal = pow((pow(x,2) + pow(y,2)), 0.5)
+    if (normal > THRESHOLD):
+        pg.draw.line(screen, RED, [origin_x + radius, origin_y + radius], 
+        [x/normal * radius + origin_x + radius, -y/normal * radius + origin_y + radius], 5)
+        text = font.render(getStringDir(), True, BLACK)
+        screen.blit(text, (origin_x + radius - text.get_rect().width/2, origin_y + radius - text.get_rect().height/2))
+    
+
+def drawDepth(screen, origin_x, origin_y, width, height):
+    global z
+    pg.draw.rect(screen, BLACK, [origin_x, origin_y, width, height], 2)
+    if (z < 0):
+        pg.draw.rect(screen, RED, [origin_x, origin_y + height / 2, width, -z * (height / 2)])
+    if (z > 0):
+        pg.draw.rect(screen,RED, [origin_x, origin_y + height / 2 - (z * height / 2), width , z * height / 2])
+    if (z != 0):
+        text = font.render(str(int(z * 10)), True, BLACK)
+        screen.blit(text, (origin_x + width / 2 - text.get_rect().width/2, origin_y + height / 2 - text.get_rect().height/2))
+
+def drawDirModule(screen, origin_x, origin_y, size):
+    drawDirection(screen, origin_x, origin_y, size)
+    drawSpeed(screen, origin_x + size * 2.5, origin_y, size / 2, size * 2)
+
+def drawRotation(screen, origin_x, origin_y, width, height):
+    global rotation
+    pg.draw.rect(screen, BLACK, [origin_x, origin_y, width, height], 2)
+    if(rotation < 0):
+        pg.draw.rect(screen, GREEN, [origin_x + width / 2 + rotation * width / 2, origin_y, -rotation * width / 2, height])
+    if(rotation > 0):
+        pg.draw.rect(screen, GREEN, [origin_x + width / 2, origin_y, rotation * width / 2, height])
+    if (rotation != 0):
+        text = font.render(str(int(rotation * 10)), True, BLACK)
+        screen.blit(text, (origin_x + width / 2 - text.get_rect().width/2, origin_y + height / 2 - text.get_rect().height/2))
+
+def drawSpeed(screen, origin_x, origin_y, width, height):
+    global speed, font
+    pg.draw.rect(screen, BLACK, [origin_x, origin_y, width, height], 5)
+    if (speed != 0):
+        pg.draw.rect(screen,RED, [origin_x, origin_y + height - (speed * height), width, speed * height])
+        text = font.render(str(int(speed * 10)), True, BLACK)
+        screen.blit(text, (origin_x + width / 2 - text.get_rect().width/2, origin_y + height / 2 - text.get_rect().height/2))
+
+
+
+
+def keepValuesAtZero():
+    global x, y, z, speed, rotation
+    if (abs(x) < THRESHOLD):
+        x = 0
+    if (abs(y) < THRESHOLD):
+        y = 0
+    if (abs(z) < THRESHOLD):
+        z = 0
+    if (abs(speed) < THRESHOLD):
+        speed = 0
+    if (abs(rotation) < THRESHOLD):
+        rotation = 0
+  
 			
 def main():
-    global joystick
+    global joystick, x, y, z, speed, rotation
     global SCREEN_SIZE
     joystick = joy_init()
     screen = pg.display.set_mode(SCREEN_SIZE, pg.RESIZABLE)
@@ -118,7 +210,7 @@ def main():
         screen.fill(WHITE)
         
         update_values()
-
+        """
         if joystickEnabled:
             guiPrint.out(screen, "Joystick Enabled")
         else: 
@@ -126,14 +218,22 @@ def main():
        
         guiPrint.out(screen, "X value:{:>6.3f}".format(x))
         guiPrint.out(screen, "Y value:{:>6.3f}".format(y))
+        guiPrint.out(screen, "Z value:{:>6.3f}".format(z))
         guiPrint.out(screen, "Speed: {:>6.3f}".format(speed))
         guiPrint.out(screen, "Rotation: {:>6.3f}".format(rotation))
         
-        #rc.setMotors(x, y, speed, rotation)
+        #rc.setMotors(x, y, z, rotation)
         
         guiPrint.out(screen, "Sensor: {}".format(rc.sensors[0]))
         guiPrint.out(screen, "Sensor: {}".format(rc.sensors[1]))
-		
+		"""
+        #drawDirection(screen, 850, 150, 100)
+        #drawSpeed(screen, 1075, 150, 50, 200)
+        drawDirModule(screen, 700, 100, 100)
+        drawDepth(screen, 1050, 100, 50, 350)
+        drawRotation(screen, 700, 400, 300, 50)
+       
+
         """
         buttons = joystick.get_numbuttons()
  
