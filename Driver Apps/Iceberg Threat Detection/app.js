f3496c2abbc2d036c52bc90493bbbdcdca17fddb
Jonathan Phippen <jphippen98012@gmail.com>
2015-05-26 17:34:17 -0700

    Changed map size, put map on top of canvas

--- Driver Apps/Iceberg Threat Detection/app.js
+++ Driver Apps/Iceberg Threat Detection/app.js
@@ -2,8 +2,22 @@ var app = angular.module('app', []);
 app.controller('ctrl', function($scope) {
 	$scope.dia = 0;
 	$scope.dep = 0;
+	$scope.keel = 0;
+	$scope.lat = 0;
+	$scope.lon = 0;
+	$scope.head = 0;
     $scope.vol = function() {
         var vol = Math.PI * Math.pow($scope.dia / 2, 2) * ($scope.dep / 3);
         return vol.toFixed(1);
     }
 });
+var canvas = document.getElementById("image");
+var drawing = canvas.getContext("2d");
+var img = document.getElementById("map");
+drawing.drawImage(img, 0, 0);
+drawing.moveTo(0, 0);
+drawing.lineTo(195, 259);
+drawing.moveTo(195, 0);
+drawing.lineTo(0, 259);
+drawing.lineWidth = 5;
+drawing.stroke();
