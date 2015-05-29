ae46a5708841bf5435a1090deae394dbc566d0d6
Jonathan Phippen <jphippen98012@gmail.com>
2015-05-28 20:02:11 -0700

    Fixed Map, Added threat table, basic keel calculations

--- Driver Apps/Iceberg Threat Detection/app.js
+++ Driver Apps/Iceberg Threat Detection/app.js
@@ -1,4 +1,5 @@
 var app = angular.module('app', []);
+
 app.controller('ctrl', function($scope) {
 	$scope.dia = 0;
 	$scope.dep = 0;
@@ -6,18 +7,76 @@ app.controller('ctrl', function($scope) {
 	$scope.lat = 0;
 	$scope.lon = 0;
 	$scope.head = 0;
+
     $scope.vol = function() {
-        var vol = Math.PI * Math.pow($scope.dia / 2, 2) * ($scope.dep / 3);
-        return vol.toFixed(1);
+    	if ($scope.dia != 0 && $scope.dep != 0) {
+        	var vol = Math.PI * Math.pow($scope.dia / 2, 2) * ($scope.dep / 3);
+        	return vol.toFixed(1);
+    	} else {
+    		return "Unknown";
+    	}
     }
+
+    function surface(depth) {
+    	if ($scope.keel != 0 && $scope.lat != 0 && $scope.lon != 0) {
+    		if ($scope.keel <= depth * 1.1) {
+    			return "Green";
+    		} else {
+    			return "Red";
+    		}
+    	} else {
+    		return "Unknown";
+    	}
+    }
+
+    function subsea(depth) {
+    	if ($scope.keel != 0 && $scope.lat != 0 && $scope.lon != 0) {
+    		if ($scope.keel <= depth * 1.1 || $scope.keel > depth * 0.7) {
+    			return "Green";
+    		} else if ($scope.keel > depth * 0.9 && $scope.keel <= depth * 0.7) {
+    			return "Yellow";
+    		} else {
+    			return "Red";
+    		}
+    	} else {
+    		return "Unknown";
+    	}
+    }
+
+    $scope.hibsur = function() { return surface(-78); }
+
+    $scope.hibsub = function() { return subsea(-78); }
+
+    $scope.seasur = function() { return surface(-107); }
+
+    $scope.seasub = function() { return subsea(-107); }
+
+    $scope.tersur = function() { return surface(-91); }
+
+    $scope.tersub = function() { return subsea(-91); }
+
+    $scope.hebsur = function() { return surface(-93); }
+
+    $scope.hebsub = function() { return subsea(-93); }
 });
-var canvas = document.getElementById("image");
-var drawing = canvas.getContext("2d");
-var img = document.getElementById("map");
-drawing.drawImage(img, 0, 0);
-drawing.moveTo(0, 0);
-drawing.lineTo(195, 259);
-drawing.moveTo(195, 0);
-drawing.lineTo(0, 259);
-drawing.lineWidth = 5;
-drawing.stroke();
+
+$(document).ready(function(){
+	var c = document.getElementById("image");
+	var ctx = c.getContext("2d");
+	var img = document.getElementById("map");
+	ctx.drawImage(img, 0, 0);
+	$("canvas#image").click(function(){
+		var ctx = this.getContext("2d");
+		var img = $("#map")[0];
+		ctx.drawImage(img, 0, 0);
+		ctx.rect(0, 0, 1, 1);
+		ctx.fillStyle = 'red';
+		ctx.fill();
+		ctx.moveTo(0, 0);
+		ctx.lineTo(194, 258);
+		ctx.moveTo(194, 0);
+		ctx.lineTo(0, 258);
+		ctx.lineWidth = 5;
+		ctx.stroke();
+	})
+})
